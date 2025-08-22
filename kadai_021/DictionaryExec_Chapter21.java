3f4662c403ce4ef6266645998a30030ebb5da0c0
srn0209 <seri-0209@ezweb.ne.jp>
2025-08-22 17:12:05 +0900

    Add files via upload

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,11 @@
+package kadai_21;
+
+public class DictionaryExec_Chapter21 {
+    public static void main(String[] args) {
+        Dictionary_Chapter21 myDictionary = new Dictionary_Chapter21();
+
+        String[] searchWords = { "apple", "banana", "grape", "orange" };
+
+        myDictionary.searchWords(searchWords);
+    }
+}
